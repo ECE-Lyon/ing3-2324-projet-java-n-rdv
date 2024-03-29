7627cb9014d7510c39a65644683fb10ecf12be4b
fares <fares.baly@edu.ece.fr>
2024-03-29 14:54:26 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/tp6", "root", "root")) {
 
 
